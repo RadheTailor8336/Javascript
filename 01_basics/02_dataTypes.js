967842a0c1b8e640a23390ecc7d55757f94e3510
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-04-15 09:35:15 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,29 @@
 
 // alert("hello")  we are using using node js, not browser
 
+console.log(3 
+    + 
+    3)  //code redabality should be high
+
+console.log("Radhey")
+
+let name = "Rohit"
+let age = 20
+let isLoggedIN = false
+let state ; 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefine => 
+// symbol => unique
+
+// object
+
+
+console.log(typeof "Hitesh");
+console.log(typeof age);
+console.log(typeof null); // null is a  type of object
+console.log(typeof undefined); // undefined 
