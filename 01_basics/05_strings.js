91e832a82340b408ebdc40ea0c4749032dae5bc4
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-04-19 19:32:01 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name ="hitesh"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+console.log(`HEllo my name is ${name} and my repo count  is ${repoCount}`)
+
+const gameName = new String('hiteshhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName)
+
