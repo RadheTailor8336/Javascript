12c902ddc2c19750f10e90d264c2e8cd71fb0538
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-04-23 13:46:44 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,12 @@
+const score =400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length); //convert number into string and count lengh of string , print it - 3(length)
+console.log(balance.toFixed(1));// print the decimal value according to input =100.0
+
+const otherNUmber = 23.8966
+
+console.log(otherNUmber.toPrecision())
