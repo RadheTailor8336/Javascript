6413dd9976c307e4117203ed8820d21ce8c32a97
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-05-30 18:22:23 +0000

    nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,9 +4,21 @@ console.log(score);
 const balance = new Number(100)
 console.log(balance);
 
-console.log(balance.toString().length); //convert number into string and count lengh of string , print it - 3(length)
-console.log(balance.toFixed(1));// print the decimal value according to input =100.0
+//console.log(balance.toString().length); //convert number into string and count lengh of string , print it - 3(length)
+//console.log(balance.toFixed(1));// print the decimal value according to input =100.0,here (1) is the presicion value
 
-const otherNUmber = 23.8966
+const otherNUmber = 123.8966
 
-console.log(otherNUmber.toPrecision())
+//console.log(otherNUmber.toPrecision(4))
+
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));//Format a number into a string, using the locale specific of india, output-10,00,000
+
+
+//++++++ Maths +++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));// absolute value leta hai output - 4
+console.log(Math.round(4.6));// roundof value leta hai output -5
+
