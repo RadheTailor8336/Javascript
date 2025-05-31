18f1f4c45c839b3ff5790e14f18a5bfe03910523
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-05-31 18:01:32 +0000

    Math and Num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,8 +1,8 @@
 const score =400
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
 //console.log(balance.toString().length); //convert number into string and count lengh of string , print it - 3(length)
 //console.log(balance.toFixed(1));// print the decimal value according to input =100.0,here (1) is the presicion value
@@ -18,7 +18,20 @@ const hundreds = 1000000
 
 //++++++ Maths +++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));// absolute value leta hai output - 4
-console.log(Math.round(4.6));// roundof value leta hai output -5
+// console.log(Math);
+// console.log(Math.abs(-4));// absolute value leta hai output = 4
+// console.log(Math.round(4.6));// roundof value leta hai output =5
+// console.log(Math.ceil(4.2));// top value choose krta hai output=5
+// console.log(Math.floor(4.9));// always take lowest value , decimal value nahi leta output =4
+// console.log(Math.sqrt(25)); //square root krta hai output =5
+// console.log(Math.min(4,3,6,8)); // minimum value print karta hai = 3
+// console.log(Math.max(4,3,6,8)); // maximum value print krta hai = 8
 
+console.log(Math.random()); // random value generate krta hai betwen 0 and 1
+console.log(Math.floor(Math.random()*10) +1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
