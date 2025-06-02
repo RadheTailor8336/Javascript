5968da16f02f735425e2e4d30745a4a62c96e718
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-02 06:06:33 +0000

    Dates and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -9,7 +9,27 @@ let myDate = new Date()
 
 //let myCreatedDate = new Date(2023,0,23);// In javascript,months are starts from 0 so here (2023,0,23) 0 is January.
 //console.log(myCreatedDate.toDateString()); // output = Mon Jan 23 2023
-let myCreatedDate= new Date(2023,0,23,5,3);
-console.log(myCreatedDate.toLocaleString()); //output= 1/23/2023, 5:03:00 AM
-hello
+//let myCreatedDate= new Date(2023,0,23,5,3);
+//console.log(myCreatedDate.toLocaleString()); //date or time btata hai,output= 1/23/2023, 5:03:00 AM
 
+let myCreatedDate= new Date("01-14-2023"); // output =1/14/2023, 12:00:00 AM
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now() // timestamp 
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1); // currrent  month btata hai jo ki 0 se start hote hain isliye +1
+console.log(newDate.getDay()); // current day
+console.log(newDate.getDate()); // current date
+console.log(newDate.getFullYear()); // current year
+
+//`${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
