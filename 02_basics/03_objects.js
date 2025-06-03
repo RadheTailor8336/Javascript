23f8d11c94d94b3b6dc6b509218436961e1b2551
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-03 09:27:56 +0000

    object literals

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,4 +1,33 @@
 //singleton 
+//Object.create
+
+
+
 
 //object literals
 
+const mySym = Symbol("key1") // Symbol datatype
+
+const Jsuser ={
+    name : "Radhe",
+    "full name": "Radhe Tailor ",
+    [mySym]: "mykey1",
+    age  : 18,
+    location : "Jaipur",
+    email : "radhe@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+//method to get acces the object
+console.log(Jsuser.email)
+console.log(Jsuser["email"])
+console.log(Jsuser["full name"])
+console.log(Jsuser [mySym])
+console.log(typeof mySym)
+
+Jsuser.email = "radhe@chargpt.com"// objects ki value ko change or override krte hai
+console.log(Jsuser.email)
+Object.freeze(Jsuser)
+Jsuser.email = "radhe@microsoft.com"
+console.log(Jsuser)
