8e661d9151e76b13ecc052289deffcd033a7f3c6
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-04 12:48:58 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,3 +36,46 @@ function loginUserMessage(username="sam"){
 //console.log(loginUserMessage("Radhe")); // value override ho jaegi = Radhe just logged in
 
 
+function calculateCarPrice(...num1){// it is the (...) rest operator, jo multiple values ko ek array mein daal deta hai
+    return num1
+}
+//console.log(calculateCarPrice(200,400,500)); // out put through rest operator ...num1 =[ 200, 400, 500 ]
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyObject){ // object ko function me pass kiya
+    //console.log(`User name is ${anyObject.username} and price is ${anyObject.price}`);
+
+    
+}
+
+//handleObject(user)
+
+handleObject({ // function ke under direct object passing
+    username : "sam",
+    price :399
+})
+
+const myNewArray = [200 ,400, 100 ,600]
+
+function returnSecondValue(getArray){
+     return getArray[1]
+}
+console.log(returnSecondValue([200,400,500,1000]));
+
+/*
+### 🔧 JavaScript Function (Definition)
+
+A **function** in JavaScript is a reusable block of code designed to perform a specific task.  
+It runs only when it is **called/invoked** and can accept input values called **parameters**, and may return an output.
+
+#### 🔹 Syntax:
+```javascript
+function functionName(parameter1, parameter2) {
+  // code to execute
+  return result;
+}
+  */
