ce7b97750420a389b646fa49a149442ec901196a
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-05 15:25:07 +0000

    Arrow function

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -17,6 +17,31 @@ const user ={
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);//this keyword works only on objects not on functions  
+//     console.log(this.username);//this keyword works only on objects not on functions  , op undefined
 // }
 // chai()
+// *** Note - browser ke under jo global object hain - window object
+
+const chai = ()=>{ // arrow function ()=>
+    let uesrname = "hitesh"
+    console.log(this.username); //op= undefined
+
+}
+//chai()
+
+// const addTwo = (num1, num2)=>{ // Arrow function
+//     return num1+num2
+// }
+
+//const addTwo = (num1, num2)=> /*return */ num1+num2 // is arrow function mein implicit return kiya hai jisme return likhne ki need nhi hoti agar curley breaces{} na use kre toh
+
+//const addTwo = (num1, num2)=> (num1+num2)
+//const addTwo = (num1, num2)=> (num1+num2)
+const addTwo = (num1, num2)=> ({username:"hitesh"})// object return kiya,op= { username: 'hitesh' }
+
+
+console.log(addTwo(3,4));
+
+
+//An arrow function is a concise ES6 syntax for writing anonymous function expressions using the => (fat arrow) operator.
+
