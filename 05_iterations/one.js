9efcbde1d4a578f59e241e5bd8d98b6adbdf4ad2
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-06 16:22:12 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -3,11 +3,47 @@
 for (let i = 0; i <= 10 ; i++) {
     const element = i;
     if (element == 5){
-            console.log("5 is best number");
+            //console.log("5 is best number");
             
     }
-    console.log(element);
+    //console.log(element);
         
 }
 
+for (let i = 0; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+      //  console.log(`Inner loop value: ${j} and Inner loop ${i}`);
+       // console.log(i + '*' + j + ' =' + i*j);;
+        
+    
+        
+    }
+    
+    
+}
+
 //console.log(element); // gives error due to local scope access
+
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(`Deteched 5`);
+   //      break;
+           continue;
+    }
+        console.log((`Value of i is ${index}`));
+        
+    
+}
