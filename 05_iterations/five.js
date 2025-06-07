5d8e51eb2738b0c264945e77a62be7efe15ea953
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-07 13:31:09 +0000

    foreach method

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -23,3 +23,25 @@ const coding =["js","ruby","java","python","cpp"];
 //     console.log(item,index,arr);
 // })
 
+const myCoding = [
+    
+    
+    {
+        languageName:"javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName:"java",
+        languageFileName: "java"
+    },
+    {
+        languageName:"python",
+        languageFileName: "py"
+    },
+
+]
+
+myCoding.forEach( (item)=>{
+    console.log(item.languageName);
+
+})
