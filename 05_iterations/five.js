4a35a6f80f3b624f14ef1540ea87be51d63b0389
RadheTailor8336 <146716373+RadheTailor8336@users.noreply.github.com>
2025-06-07 12:10:57 +0000

    for each

---
+++ 05_iterations/five.js
@@ -0,0 +1,25 @@
+// forEach() is an array method that runs a callback function once for each element in the array.
+// It does not return a new array, and it cannot be broken with break/continue.
+
+const coding =["js","ruby","java","python","cpp"];
+
+// coding.forEach( function (val){
+//     console.log(val);
+    
+
+// })
+
+// coding.forEach( (item)=> {
+//     console.log(item)
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach( (item,index,arr) => {
+//     console.log(item,index,arr);
+// })
+
